4e65d88588642c00a07f13cd147953c9098eaa85
SamZec <samzec4@gmail.com>
2023-11-01 21:27:46 +0000

    1. Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,7 @@
 
 
 import re
+import logging
 from typing import List
 
 
@@ -13,3 +14,22 @@ def filter_datum(fields: List[str], redaction: str,
         message = re.sub(rf"{item}=(.*?)\{separator}",
                          f'{item}={redaction}{separator}', message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Intantiate objects"""
+        self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """records loggings"""
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
