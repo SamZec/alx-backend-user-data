a16db2c2f6a1ecee0da8438cb2fa8b7a5f02c8c7
SamZec <samzec4@gmail.com>
2023-11-06 23:23:18 +0000

    8. Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,14 +3,25 @@
 
 
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
     """BasicAuth"""
-    def extract_base64_authorization_header(self,
-                                            authorization_header: str) -> str:
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
         """returns the Base64 part of the Authorization"""
         if (authorization_header and type(authorization_header) == str and
                 authorization_header.startswith('Basic ')):
             return authorization_header.split(' ')[1]
         return None
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """Basic - Base64 decode"""
+        try:
+            d_byte = base64.b64decode(base64_authorization_header,
+                                      validate=True)
+            return d_byte.decode('utf-8')
+        except Exception:
+            return None
