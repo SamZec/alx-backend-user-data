aaec527c87163580012b084f940cdd06836683d6
SamZec <samzec4@gmail.com>
2023-11-06 21:36:01 +0000

    4. Define which routes don't need authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,7 +10,12 @@ class Auth:
     """manage API authentication"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """require auth"""
-        return False
+        if not path or not excluded_paths or len(excluded_paths) == 0:
+            return True
+        if (path in excluded_paths or (path + '/') in excluded_paths or
+                path[:-1] in excluded_paths):
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """authorization header"""
