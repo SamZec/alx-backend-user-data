74a4ea9129a3846959ab8142a76e3df5d9130405
SamZec <samzec4@gmail.com>
2023-11-07 06:37:06 +0000

    10. Basic - User object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -50,7 +50,7 @@ class BasicAuth(Auth):
             user = User().search({'email': user_email})
             if not user:
                 return None
-        if not user[0].is_valid_password(user_pwd):
+        if not user or not user[0].is_valid_password(user_pwd):
             return None
         return user[0]
 
