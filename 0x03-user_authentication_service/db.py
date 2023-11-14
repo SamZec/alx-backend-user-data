f07e06a397c6c7fcf30642878c350318caf2eac5
SamZec <samzec4@gmail.com>
2023-11-14 06:57:45 +0000

    1. create user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -35,11 +35,13 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> TypeVar('User'):
         """save user to the database"""
+        if not email or not hashed_password:
+            return
         user = User(email=email, hashed_password=hashed_password)
         session = self._session
         session.add(user)
         session.commit()
-        return None
+        return user
 
 
 '''
