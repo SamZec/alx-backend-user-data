911af41ae9fd52df6329cd3035c7683038ec5270
SamZec <samzec4@gmail.com>
2023-11-14 22:22:20 +0000

    18. Update password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -92,6 +92,8 @@ class Auth:
 
     def update_password(self, reset_token: str, password: str) -> None:
         """update password"""
+        if not reset_token:
+            raise ValueError
         try:
             user = self._db.find_user_by(reset_token=reset_token)
         except NoResultFound:
